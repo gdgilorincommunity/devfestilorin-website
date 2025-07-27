328e8a9f1c35b98bb75158f320d884b6771241a8
Ahmed Olanrewaju <olanrewajuahmed095@yahoo.com>
2025-07-27 16:14:23 +0100

    fix: update lint command

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,19 +1,27 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'header-max-length': [2, 'always', 72],
-        'subject-case': [2, 'always', ['lower-case', 'camel-case', 'sentence-case']],
-        'type-enum': [2, 'always', [
-            'build',
-            'chore',
-            'ci',
-            'docs',
-            'feat',
-            'fix',
-            'perf',
-            'refactor',
-            'style',
-            'test'
-        ]],
-    },
-};
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [
+      2,
+      'always',
+      ['lower-case', 'camel-case', 'sentence-case'],
+    ],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'style',
+        'test',
+      ],
+    ],
+  },
+}

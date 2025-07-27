499e9cc5c528976394a5d626704b9542735513ae
Ahmed Olanrewaju <olanrewajuahmed095@yahoo.com>
2025-07-27 17:27:23 +0100

    fix: setup query provider and main app files

---
+++ src/app/query-provider.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import { getQueryClient } from './get-query-client'
+
+interface Props {
+  children: React.ReactNode
+}
+
+export default function QueryProvider({ children }: Props) {
+  const queryClient = getQueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
+}

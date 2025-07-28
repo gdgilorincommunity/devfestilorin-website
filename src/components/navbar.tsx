cc9580a80c31653d1a0e793c11ed1b47f5100e38
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2025-07-28 21:24:22 +0100

    feat: update layout, page, navbar, and ui components

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,8 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import React from 'react'
+
+import { Button } from '@/components/ui/button'
 import {
   Sheet,
   SheetTrigger,
@@ -5,10 +10,6 @@ import {
   SheetTitle,
   SheetDescription,
 } from '@/components/ui/sheet'
-import { Button } from '@/components/ui/button'
-import Link from 'next/link'
-import Image from 'next/image'
-import React from 'react'
 import { menuLinks } from '@/constants/menuLinks'
 
 export default function Navbar() {
@@ -16,7 +17,7 @@ export default function Navbar() {
     <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
       <Sheet>
         <SheetTrigger asChild>
-          <Button variant="outline" size="icon" className="lg:hidden">
+          <Button className="lg:hidden" size="icon" variant="outline">
             <MenuIcon className="h-6 w-6" />
             <span className="sr-only">Toggle navigation menu</span>
           </Button>
@@ -26,20 +27,20 @@ export default function Navbar() {
           <SheetDescription className="sr-only">
             Access navigation links and menu options
           </SheetDescription>
-          <Link href="#" className="mr-6 hidden lg:flex" prefetch={false}>
+          <Link className="mr-6 hidden lg:flex" href="#" prefetch={false}>
             <Image
-              src="/devfest-ilorin.svg"
               alt="logo"
-              width={100}
               height={100}
+              src="/devfest-ilorin.svg"
+              width={100}
             />
           </Link>
           <div className="grid gap-2 py-6 p-4">
             {menuLinks.map((item) => (
               <Link
                 key={item.path}
-                href={item.path}
                 className="flex w-full items-center py-2 text-lg font-semibold"
+                href={item.path}
                 prefetch={false}
               >
                 {item.title}
@@ -48,22 +49,22 @@ export default function Navbar() {
           </div>
         </SheetContent>
       </Sheet>
-      <Link href="#" className="mr-6 hidden lg:flex" prefetch={false}>
-        <Image src="/devfest-ilorin.svg" alt="logo" width={150} height={150} />
+      <Link className="mr-6 hidden lg:flex" href="#" prefetch={false}>
+        <Image alt="logo" height={150} src="/devfest-ilorin.svg" width={150} />
       </Link>
       <nav className="ml-auto hidden lg:flex gap-6 items-center">
         {menuLinks.map((item) => (
           <Link
             key={item.path}
-            href={item.path}
             className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            href={item.path}
             prefetch={false}
           >
             {item.title}
           </Link>
         ))}
 
-        <Link href="/register" className="ml-6">
+        <Link className="ml-6" href="/register">
           <Button showArrow>Get Tickets</Button>
         </Link>
       </nav>
@@ -75,15 +76,15 @@ function MenuIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
       {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
       fill="none"
+      height="24"
       stroke="currentColor"
-      strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      width="24"
+      xmlns="http://www.w3.org/2000/svg"
     >
       <line x1="4" x2="20" y1="12" y2="12" />
       <line x1="4" x2="20" y1="6" y2="6" />

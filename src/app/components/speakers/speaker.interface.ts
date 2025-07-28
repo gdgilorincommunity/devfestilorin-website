1acf42e1288e4e8c97f23ff2b22b6d39328bec15
Daniel Olabemiwo <olabemiwodaniel@gmail.com>
2025-07-28 22:57:22 +0100

    feat: speaker section

---
+++ src/app/components/speakers/speaker.interface.ts
@@ -0,0 +1,10 @@
+interface Speaker {
+    id: number;
+    name: string;
+    image: string;
+    description: string;
+    title: string;
+    company: string;
+    link: string;
+    type: string;
+}

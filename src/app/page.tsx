62ae36b6521f91a4174abcd5a3643561c9d4a1ca
Ahmed Olanrewaju <olanrewajuahmed095@yahoo.com>
2025-07-29 00:01:06 +0100

    chore: cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Hero from '@/components/hero'
+import SpeakerSection from '@/components/speakers/speakers-section'
+import SponsorSection from '@/components/sponsors/sponsors-section'
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <SpeakerSection />
+      <SponsorSection />
     </div>
   )
 }

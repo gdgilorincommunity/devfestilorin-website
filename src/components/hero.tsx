f2a9c3d781bd62a8b8c4ff7e2d5e7101e3db198d
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2025-07-29 12:58:34 +0100

    fix: update z-index in hero component and add cursor style for better user interaction

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -233,7 +233,7 @@ const Hero = () => {
         </div>
       </section>
 
-      <div className="absolute inset-0 pointer-events-none z-20">
+      <div className="absolute inset-0 z-50">
         <Gravity
           ref={gravityRef}
           autoStart={false}
@@ -249,7 +249,7 @@ const Hero = () => {
               y={tag.y}
             >
               <div
-                className={`font-bold text-base sm:text-lg md:text-xl lg:text-2xl ${tag.bgColor} rounded-full hover:cursor-grab px-3.5 sm:px-5 md:px-7 py-2 sm:py-2.5 md:py-3.5 border-black border-2 text-[#1E1E1E]`}
+                className={`font-bold text-base sm:text-lg md:text-xl lg:text-2xl ${tag.bgColor} rounded-full hover:cursor-grab px-3.5 sm:px-5 md:px-7 py-2 sm:py-2.5 md:py-3.5 border-black border-2 text-[#1E1E1E] cursor-grab`}
               >
                 {tag.text}
               </div>

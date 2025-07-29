a81e4f96ba60e21dc8b5c4626f78057fa192de2c
Abdul-Qawi Laniyan <oladotunlaniyan@gmail.com>
2025-07-29 02:34:03 +0100

    feat: venue and event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Hero from '@/components/hero'
+import Events from '@/components/events'
+import Venue from '@/components/venue'
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Events />
+      <Venue />
     </div>
   )
 }
